6c05cfcedf9bc002be456cc7f94597b4d2db624a
jehaddayman <jehaddayman@gmail.com>
2025-08-12 00:02:21 +0300

    Create [id].tsx

---
+++ pages/property/[id].tsx
@@ -0,0 +1,19 @@
+import { PROPERTYLISTINGSAMPLE } from "@/constants";
+import { useRouter } from "next/router";
+import PropertyDetail from "@/components/property/PropertyDetail";
+
+export default function PropertyPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const property = PROPERTYLISTINGSAMPLE.find(
+    (item) => item.name.replace(/\s+/g, "-").toLowerCase() === String(id).toLowerCase()
+  );
+
+  if (!property) return <p>Property not found</p>;
+
+  return (
+    <div>
+      <PropertyDetail property={property} />
+    </div>
+  );
+}
